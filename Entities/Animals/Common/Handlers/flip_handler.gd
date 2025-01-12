6c3715383eed18b74627dc3d2431da04457177a8
Jean-Marc Strauven <jms@grazulex.be>
2025-01-12 10:37:52 +0100

    add FlipHandler

--- Entities/Animals/Common/Handlers/flip_handler.gd
+++ Entities/Animals/Common/Handlers/flip_handler.gd
@@ -1,7 +1,12 @@
 extends Node2D
 class_name FlipHandler
 
+var last_velocity_x : int = 0
+
 func handle_flip(character_body: CharacterBody2D) -> void:
+	if last_velocity_x == character_body.velocity.x:
+		return
+		
 	if character_body.velocity.x < 0 :
 		if character_body.variants.is_flipped:
 			character_body.sprite_2d.flip_h = true
